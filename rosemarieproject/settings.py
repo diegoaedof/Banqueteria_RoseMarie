f31dd2c1132feb8e37f1d9b62924ff93bd7c8abd
diegoaedof <d.aedo.1108@gmail.com>
2023-11-02 23:21:14 -0300

    instalación de apps en settings.py

--- rosemarieproject/settings.py
+++ rosemarieproject/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'publico',
+    'gestor_agenda',
+    'gestor_productos',
+    'gestor_cotizaciones',
+    'sistema_administrador',
 ]
 
 MIDDLEWARE = [
