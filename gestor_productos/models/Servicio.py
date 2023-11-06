38de57c3ba799093fbd8c9138f972e656c4c371b
diegoaedof <d.aedo.1108@gmail.com>
2023-11-06 00:13:34 -0300

    PI-133

---
+++ gestor_productos/models/Servicio.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Servicio(models.Model):
+    LISTA_UNIDADES_DE_MEDIDA = [
+        ("DD","Dias"),
+        ("hh","Horas"),
+        ("u.","unidades")
+    ]
+    nombre = models.CharField("Nombre del Servicio", max_length = 100, unique=True)
+    unidad_de_medida = models.CharField(max_length=2, choices=LISTA_UNIDADES_DE_MEDIDA)
+    valor_por_unidad = models.FloatField()
+    def __str__(self):
+        return self.nombre
