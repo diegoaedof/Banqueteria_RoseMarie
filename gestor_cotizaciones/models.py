3fb3f97a09cad539ecc6d25dbc4fb0a1d0cfb8f6
diegoaedof <d.aedo.1108@gmail.com>
2023-11-06 19:25:32 -0300

    avance PI-106

--- gestor_cotizaciones/models.py
+++ gestor_cotizaciones/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+#Resolver herencia para el formulario
+class MensajeFormulario(models.Model):
+    nombre = models.CharField("Nombre del Cliente", max_length=30)
+    apellidos = models.CharField("Apellidos del Cliente", max_length=50)
+    asunto = models.TextField("Asunto o título del mensaje", max_length=2000)
+    tipo_de_mensaje = models.TextChoices("Tipo de Mensaje","SOLICITUD_DE_COTIZACION CONSULTA SUGERENCIA FELICITACIONES")
+
+class SolicitudCotizacion(MensajeFormulario):
+    tipo_de_evento = models.IntegerChoices("Tipo de Evento", "MATRIMONIO CUMPLEAÑOS GALA_4TO_MEDIO")
+    cantidad_de_personas = models.IntegerField()
+    servicios = models.CharField(max_length=200)
+    productos = models.CharField()
+    fechas = models.DateField()
+
+
+
+
