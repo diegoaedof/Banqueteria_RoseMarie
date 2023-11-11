bc8595475d9b888e8d9bd4d7e64c33549f1714a7
diegoaedof <d.aedo.1108@gmail.com>
2023-11-11 01:16:54 -0300

    comentario en gc/models

--- gestor_cotizaciones/models.py
+++ gestor_cotizaciones/models.py
@@ -6,6 +6,7 @@ class MensajeFormulario(models.Model):
     nombre = models.CharField("Nombre del Cliente", max_length=30)
     apellidos = models.CharField("Apellidos del Cliente", max_length=50)
     asunto = models.TextField("Asunto o título del mensaje", max_length=2000)
+    #modificar como el modelo de Producto
     tipo_de_mensaje = models.TextChoices("Tipo de Mensaje", "SOLICITUD_DE_COTIZACION CONSULTA SUGERENCIA FELICITACIONES")
     email = models.EmailField()
     telefono = models.CharField(max_length=11)
@@ -15,6 +16,6 @@ class SolicitudCotizacion(MensajeFormulario):
     tipo_de_evento = models.IntegerChoices("Tipo de Evento", "MATRIMONIO CUMPLEAÑOS GALA_4TO_MEDIO")
     cantidad_de_personas = models.IntegerField()
     servicios = models.CharField(max_length=200)
-    productos = models.CharField(max_length=200)
-    fechas = models.DateField()
+    fecha = models.DateField()
+    hora = models.TimeField()
 
