53608f5ac7d0928b4f47d5f2bf4cc74741505d30
diegoaedof <d.aedo.1108@gmail.com>
2023-11-10 23:27:55 -0300

    arreglando la cagá del yerko

---
+++ sistema_administrador/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('panel_administracion', views.panel_administracion, name='panel_administracion'),
+    path('productos', views.productos, name='productos'),
+    path('mi_perfil', views.mi_perfil, name='mi_perfil'),
+    path('factura', views.factura, name='factura'),
+    path('cotizaciones', views.cotizaciones, name='cotizaciones'),
+    path('agenda', views.agenda, name='agenda'),
+    path('clientes', views.clientes, name='clientes'),
+]
