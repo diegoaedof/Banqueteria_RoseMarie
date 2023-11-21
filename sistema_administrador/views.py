d8d29b414f91e6d2f8912f17f3cb2ede28136393
Yerko <y.arancibia.labra@gmail.com>
2023-11-20 22:54:28 -0300

    Subiendo todo lo que tengo

--- sistema_administrador/views.py
+++ sistema_administrador/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ProductoForm, ServicioForm
+from .models import Producto, Servicio
 
 def panel_administracion(request):
     context= {}
@@ -28,4 +30,33 @@ def productos(request):
     context = {}
     return render(request, 'productos.html', context)
 
+def productos_stock(request):
+    productos = Producto.objects.all()
+    return render(request, 'productos_stock.html', {'productos': productos})
 
+def productos_add(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('productos_stock')  # Redirect to a success page
+    else:
+        form = ProductoForm()
+
+    context = {'form': form}
+    return render(request, 'productos_add.html', context)
+
+def servicios(request):
+    servicios = Servicio.objects.all()
+    return render(request, 'servicios.html',  {'servicios': servicios})
+
+def servicios_add(request):
+    if request.method == 'POST':
+        form = ServicioForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('servicios')  # Reemplaza 'lista_servicios' con el nombre de tu vista de lista de servicios
+    else:
+        form = ServicioForm()
+
+    return render(request, 'servicios_add.html', {'form': form})
